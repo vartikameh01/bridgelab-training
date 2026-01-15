0d35da57fa109987eb95c14b630e04f1769bc467
Vartika <mehrotravartika76@gmail.com>
2026-01-15 14:38:44 +0530

    commit

---
+++ string/gitbranch/firstcode.js
@@ -0,0 +1,15 @@
+//1.Hello World  //single line comment use for explain the code
+console.log("Node.js Backend Started");  //console.log() is used to print output on the terminal/console.
+// "Node.js Backend Started" is a string message.
+
+//Variables 
+let name="Mahak";  //string   //use to declare variables and value inside it cal bhe change
+let age=21;        //number
+const country="India";  //constant    //use to declare the varianbles
+let isStudent=true;     //boolean      //use to prints a data in boolean form (true/false)
+
+console.log(name);
+console.log(age);
+console.log(country);
+console.log(isStudent);
+
